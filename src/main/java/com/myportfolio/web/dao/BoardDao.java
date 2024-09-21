d35b0f45127e7ba298d75aeacd338af6cf2a1bb9
Min Woo Jeon <jeonminwoo2000@gmail.com>
2024-09-21 13:24:36 +0900

    Add: BoardDao 작성및 , mapper sql작성

---
+++ src/main/java/com/myportfolio/web/dao/BoardDao.java
@@ -0,0 +1,20 @@
+package com.myportfolio.web.dao;
+
+import com.myportfolio.web.domain.*;
+
+import java.util.*;
+
+public interface BoardDao {
+    BoardDto select(Integer bno) throws Exception;
+    int delete(Integer bno, String writer) throws Exception;
+    int insert(BoardDto dto) throws Exception;
+    int update(BoardDto dto) throws Exception;
+    int increaseViewCnt(Integer bno) throws Exception;
+
+    List<BoardDto> selectPage(Map map) throws Exception;
+    List<BoardDto> selectAll() throws Exception;
+    int deleteAll() throws Exception;
+    int count() throws Exception;
+
+
+}
